19b56391ac64a8b52109e0cb0e30c4d36b8e8cc8
Michael Wayne Goodman <goodman.m.w@gmail.com>
2019-05-22 08:17:37 +0800

    Work on util/corpus-stats.py

--- util/corpus-stats.py
+++ util/corpus-stats.py
@@ -9,6 +9,9 @@ import norman
 def main(args):
     with open(args.file) as fh:
         gs = list(norman.robust_load(fh.read()))
+    re_map = norman.load_reifications(args.reify)
+    co_map = norman.load_dereifications(args.collapse)
+
     ostats = [(len(g.variables()), len(g.triples())) for g in gs]
     print('Original:')
     print('  Average / Total number of nodes: {:.2f} / {:d}'
@@ -18,16 +21,12 @@ def main(args):
           .format(statistics.mean(e for _, e in ostats),
                   sum(e for _, e in ostats)))
 
-    re_map = norman.load_reifications(args.reify)
-    co_map = norman.load_dereifications(args.collapse)
-
-    gs_ = [norman.reify(g, re_map) for g in gs]
-    assert len(gs_) == len(gs)
-    diff = sum(1 if gs_[i].variables() != gs[i].variables() else 0
-               for i in range(len(gs_)))
-    gs = gs_
-    ostats = [(len(g.variables()), len(g.triples())) for g in gs]
-    print('Reified ({} / {})'.format(diff, len(gs)))
+    rgs = [norman.reify(g, re_map) for g in gs]
+    assert len(rgs) == len(gs)
+    diff = sum(1 if rgs[i].variables() != gs[i].variables() else 0
+               for i in range(len(rgs)))
+    ostats = [(len(g.variables()), len(g.triples())) for g in rgs]
+    print('Reified ({} / {})'.format(diff, len(rgs)))
     print('  Average / Total number of nodes: {:.2f} / {:d}'
           .format(statistics.mean(n for n, _ in ostats),
                   sum(n for n, _ in ostats)))
@@ -35,13 +34,25 @@ def main(args):
           .format(statistics.mean(e for _, e in ostats),
                   sum(e for _, e in ostats)))
 
-    gs_ = [norman.collapse(g, co_map) for g in gs]
-    assert len(gs_) == len(gs)
-    diff = sum(1 if gs_[i].variables() != gs[i].variables() else 0
-               for i in range(len(gs_)))
-    gs = gs_
-    ostats = [(len(g.variables()), len(g.triples())) for g in gs]
-    print('Collapsed ({} / {})'.format(diff, len(gs)))
+    cgs = [norman.collapse(g, co_map) for g in gs]
+    crs = [norman.collapse(g, co_map) for g in rgs]
+    assert len(cgs) == len(gs)
+    assert len(crs) == len(gs)
+    diff = sum(1 if cgs[i].variables() != gs[i].variables() else 0
+               for i in range(len(cgs)))
+    ostats = [(len(g.variables()), len(g.triples())) for g in cgs]
+    print('Collapsed from original ({} / {})'.format(diff, len(gs)))
+    print('  Average / Total number of nodes: {:.2f} / {:d}'
+          .format(statistics.mean(n for n, _ in ostats),
+                  sum(n for n, _ in ostats)))
+    print('  Average / Total number of edges: {:.2f} / {:d}'
+          .format(statistics.mean(e for _, e in ostats),
+                  sum(e for _, e in ostats)))
+
+    diff = sum(1 if crs[i].variables() != rgs[i].variables() else 0
+               for i in range(len(crs)))
+    ostats = [(len(g.variables()), len(g.triples())) for g in crs]
+    print('Collapsed from reified ({} / {})'.format(diff, len(gs)))
     print('  Average / Total number of nodes: {:.2f} / {:d}'
           .format(statistics.mean(n for n, _ in ostats),
                   sum(n for n, _ in ostats)))
