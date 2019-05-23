6be59eb2dd5858ed484fb7a410ede4d1bde7d9c8
Michael Wayne Goodman <goodman.m.w@gmail.com>
2019-05-24 00:59:28 +0800

    Tried to evaluate from run.sh; incomplete.

--- run.sh
+++ run.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-DATA=data
+# usage: run.sh <gold-file> <sys-file> [<sys-file>...]
+
+if [ $# -lt 2 -o ! -f "$1" -o ! -f "$2" ]; then
+    echo "usage: run.sh <gold-file> <sys-file> [<sys-file>...]"
+    exit 1
+fi
+
 OUT=out
-
-mkdir -p "$OUT"
-
+EVAL=~/repos/amr-evaluation/evaluation.sh
 export PYTHONPATH="$PYTHONPATH":~/repos/penman
 
-for path in "${DATA}"/*.txt; do
+gold="$1"; shift
+mkdir -p "$OUT"
+
+for path in "$@"; do
+    [ -f "$path" ] || { echo "skipping invalid file: $path"; continue }
     f=$(basename "${path%%.txt}")
     echo "Normalizing $f" 1>&2
+    sed '/^#/d' "$path" > out/"$f.raw.txt"
     python norman.py "$path" > out/"$f.norm.txt"
+    python norman.py --conceptualize "$path" > out/"$f.co.txt"
     python norman.py --reify maps/reifications.tsv "$path" > out/"$f.re.txt"
     python norman.py --collapse maps/dereifications.tsv "$path" > out/"$f.de.txt"
-    python norman.py --collapse maps/dereifications.tsv "out/$f.re.txt" > out/"$f.rede.txt"
+    python norman.py --collapse maps/dereifications.tsv "out/$f.re.txt" > out/"$f.re.de.txt"
+    python norman.py --reify maps/reifications.tsv --conceptualize "$path" > out/"$f.re.co.txt"
 done
