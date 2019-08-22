45a6030e426c7fb551c9aa483d943b4346a09a3f
Michael Wayne Goodman <goodman.m.w@gmail.com>
2019-08-22 16:46:13 +0800

    Clean up code and specify dependencies.

--- run.sh
+++ run.sh
@@ -19,14 +19,41 @@ EOF
 
 # redefine this as necessary
 evaluate() {
-    ~/repos/smatch/smatch.py --pr -f "$2" "$1"
+    python -m smatch --pr -f "$2" "$1"
 }
 
 DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 OUT="$DIR"/out
-export PYTHONPATH="$PYTHONPATH":~/repos/penman
 NORM=false
 EVAL=false
+
+declare -A NORMS
+NORMS[i]="--canonical-role-inversion"
+NORMS[r]="--reify maps/reifications.tsv"
+NORMS[a]="--reify-attributes"
+NORMS[p]="--preserve-structure"
+NORMS[c]="--collapse maps/dereifications.tsv"
+
+# these are the keys of tests that are actually run
+TESTS=(
+    'i'
+    'r'
+    'a'
+    'p'
+    'i r'
+    'i a'
+    'i p'
+    'r a'
+    'r p'
+    'a p'
+    'i r a'
+    'i r p'
+    'r a p'
+    'i r a p'
+)
+
+makeopts() { for k in $1; do printf " %s" ${NORMS[$k]}; done; }
+suffix() { sed 's/ \+/./g' <<< "$1"; }
 
 while [ $# -gt 0 ]; do
     case "$1" in
@@ -52,95 +79,19 @@ if [ "$NORM" == "true" ]; then
         [ -f "$path" ] || { echo "skipping invalid file: $path"; continue; }
         f=$(basename "${path%%.txt}")
         echo "Normalizing $f" 1>&2
+
+	# get clean file without comments
         sed '/^#/d' "$path" > "$OUT"/"$f.raw.txt"
-        python norman.py \
-	       "$path" \
-	       > "$OUT"/"$f.norm.txt"
-        python norman.py \
-	       "$path" \
-	       --canonical-role-inversion \
-	       > "$OUT"/"$f.i.txt"
-        python norman.py \
-	       --conceptualize \
-	       "$path" \
-	       > "$OUT"/"$f.a.txt"
-        python norman.py \
-	       --reify maps/reifications.tsv \
-	       "$path" \
-	       > "$OUT"/"$f.r.txt"
-        python norman.py \
-	       --collapse maps/dereifications.tsv \
-	       "$path" \
-	       > "$OUT"/"$f.d.txt"
-        python norman.py \
-	       --node-tops \
-	       "$path" \
-	       > "$OUT"/"$f.t.txt"
-        python norman.py \
-	       --collapse maps/dereifications.tsv \
-	       "$OUT"/"$f.r.txt" \
-	       > "$OUT"/"$f.r.d.txt"
-        python norman.py \
-	       --canonical-role-inversion \
-	       --conceptualize \
-	       "$path" \
-	       > "$OUT"/"$f.i.a.txt"
-        python norman.py \
-	       --canonical-role-inversion \
-	       --reify maps/reifications.tsv \
-	       "$path" \
-	       > "$OUT"/"$f.i.r.txt"
-        python norman.py \
-	       --canonical-role-inversion \
-	       --node-tops \
-	       "$path" \
-	       > "$OUT"/"$f.i.t.txt"
-        python norman.py \
-	       --conceptualize \
-	       --reify maps/reifications.tsv \
-	       "$path" \
-	       > "$OUT"/"$f.a.r.txt"
-        python norman.py \
-	       --conceptualize \
-	       --node-tops \
-	       "$path" \
-	       > "$OUT"/"$f.a.t.txt"
-        python norman.py \
-	       --reify maps/reifications.tsv \
-	       --node-tops \
-	       "$path" \
-	       > "$OUT"/"$f.r.t.txt"
-        python norman.py \
-	       --canonical-role-inversion \
-	       --reify maps/reifications.tsv \
-	       --conceptualize \
-	       "$path" \
-	       > "$OUT"/"$f.i.a.r.txt"
-        python norman.py \
-	       --canonical-role-inversion \
-	       --conceptualize \
-	       --node-tops \
-	       "$path" \
-	       > "$OUT"/"$f.i.a.t.txt"
-        python norman.py \
-	       --canonical-role-inversion \
-	       --reify maps/reifications.tsv \
-	       --node-tops \
-	       "$path" \
-	       > "$OUT"/"$f.i.r.t.txt"
-        python norman.py \
-	       --conceptualize \
-	       --reify maps/reifications.tsv \
-	       --node-tops \
-	       "$path" \
-	       > "$OUT"/"$f.a.r.t.txt"
-        python norman.py \
-	       --canonical-role-inversion \
-	       --conceptualize \
-	       --reify maps/reifications.tsv \
-	       --node-tops \
-	       "$path" \
-	       > "$OUT"/"$f.i.a.r.t.txt"
+	# sanity check that the normalizer doesn't change things
+	# without requested normalizations
+	python norman.py "$path" > "$OUT"/"$f.norm.txt"
+
+	for x in "${TESTS[@]}"; do
+	    suf=$( suffix "$x" )
+	    python norman.py $( makeopts "$x" ) \
+		   "$path" \
+		   > "$OUT/$f.$suf.txt"
+	done
     done
 fi
 
@@ -148,17 +99,12 @@ if [ "$EVAL" == "true" ]; then
     g=$(basename "${1%%.txt}")
     shift  # remove gold path from "$@"
 
-    echo "Evaluating $g to itself (attr-re)"
-    evaluate "$OUT"/"$g.raw.txt" "$OUT"/"$g.a.txt" \
-	     > "$OUT"/"$g.a.eval"
-
-    echo "Evaluating $g to itself (relation-re)"
-    evaluate "$OUT"/"$g.raw.txt" "$OUT"/"$g.r.txt" \
-	     > "$OUT"/"$g.r.eval"
-
-    echo "Evaluating $g to itself (attr-re and relation-re)"
-    evaluate "$OUT"/"$g.raw.txt" "$OUT"/"$g.a.r.txt" \
-	     > "$OUT"/"$g.a.r.eval"
+    for x in "${TESTS[@]}"; do
+	suf=$( suffix "$x" )
+	echo "Evaluating $g to itself ($suf)"
+	evaluate "$OUT/$g.raw.txt" "$OUT/$g.$suf.txt" \
+		 > "$OUT/$g.$suf.eval"
+    done
 
     for sys in "$@"; do
         s=$(basename "${sys%%.txt}")
@@ -171,56 +117,11 @@ if [ "$EVAL" == "true" ]; then
         evaluate "$OUT"/"$g.norm.txt" "$OUT"/"$s.norm.txt" \
 		 > "$OUT"/"$s.norm.eval"
 
-        echo "Evaluating $g and $s (inv)"
-        evaluate "$OUT"/"$g.i.txt" "$OUT"/"$s.i.txt" \
-		 > "$OUT"/"$s.i.eval"
-
-        echo "Evaluating $g and $s (attr-re)"
-        evaluate "$OUT"/"$g.a.txt" "$OUT"/"$s.a.txt" \
-		 > "$OUT"/"$s.a.eval"
-
-        echo "Evaluating $g and $s (relation-re)"
-        evaluate "$OUT"/"$g.r.txt" "$OUT"/"$s.r.txt" \
-		 > "$OUT"/"$s.r.eval"
-
-        echo "Evaluating $g and $s (topped)"
-        evaluate "$OUT"/"$g.t.txt" "$OUT"/"$s.t.txt" \
-		 > "$OUT"/"$s.t.eval"
-
-        echo "Evaluating $g and $s (inv and attr-re)"
-        evaluate "$OUT"/"$g.i.a.txt" "$OUT"/"$s.i.a.txt" \
-		 > "$OUT"/"$s.i.a.eval"
-
-        echo "Evaluating $g and $s (inv and relation-re)"
-        evaluate "$OUT"/"$g.i.r.txt" "$OUT"/"$s.i.r.txt" \
-		 > "$OUT"/"$s.i.r.eval"
-
-        echo "Evaluating $g and $s (inv and topped)"
-        evaluate "$OUT"/"$g.i.t.txt" "$OUT"/"$s.i.t.txt" \
-		 > "$OUT"/"$s.i.t.eval"
-
-        echo "Evaluating $g and $s (attr-re and relation-re)"
-        evaluate "$OUT"/"$g.a.r.txt" "$OUT"/"$s.a.r.txt" \
-		 > "$OUT"/"$s.a.r.eval"
-
-        echo "Evaluating $g and $s (inv, attr-re, and relation-re)"
-        evaluate "$OUT"/"$g.i.a.r.txt" "$OUT"/"$s.i.a.r.txt" \
-		 > "$OUT"/"$s.i.a.r.eval"
-
-        echo "Evaluating $g and $s (inv, attr-re, and topped)"
-        evaluate "$OUT"/"$g.i.a.t.txt" "$OUT"/"$s.i.a.t.txt" \
-		 > "$OUT"/"$s.i.a.t.eval"
-
-        echo "Evaluating $g and $s (inv, relation-re, and topped)"
-        evaluate "$OUT"/"$g.i.r.t.txt" "$OUT"/"$s.i.r.t.txt" \
-		 > "$OUT"/"$s.i.r.t.eval"
-
-        echo "Evaluating $g and $s (attr-re, relation-re, and topped)"
-        evaluate "$OUT"/"$g.a.r.t.txt" "$OUT"/"$s.a.r.t.txt" \
-		 > "$OUT"/"$s.a.r.t.eval"
-
-        echo "Evaluating $g and $s (inv, attr-re, relation-re, and topped)"
-        evaluate "$OUT"/"$g.i.a.r.t.txt" "$OUT"/"$s.i.a.r.t.txt" \
-		 > "$OUT"/"$s.i.a.r.t.eval"
+	for x in "${TESTS[@]}"; do
+	    suf=$( suffix "$x" )
+	    echo "Evaluating $g and $s ($suf)"
+	    evaluate "$OUT/$g.$suf.txt" "$OUT/$s.$suf.txt" \
+		     > "$OUT/$s.$suf.eval"
+	done
     done
 fi
--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
+import os
 from setuptools import setup
 
+base_dir = os.path.dirname(__file__)
+with open(os.path.join(base_dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='norman',
     version='0.1.0',
     description='AMR Normalization',
-    long_description='',
-    url='',
-    author='',
-    author_email='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/goodmami/norman',
+    author='Michael Wayne Goodman',
+    author_email='goodman.m.w@gmail.com',
     license='MIT',
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -20,22 +25,19 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing :: Linguistic',
         'Topic :: Utilities'
     ],
-    keywords='nlp amr',
+    keywords='nlp amr semantics graphs',
     py_modules=['norman'],
+    setup_requires=['wheel >= 0.31.0'],
     install_requires=[
-        'penman >= 0.6.2'
+        'penman == 0.6.2',
+        'smatch == 1.0.1'
     ]
-    # entry_points={
-    #     'console_scripts': [
-    #         'penman=...'
-    #     ]
-    # }
 )
